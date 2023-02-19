056d2bcfffeaf21d4e34dcf5a6a46755ce0966b3
Magdalena Sędkiewicz <magdalenainspirations@gmail.com>
2023-02-19 20:34:17 +0100

    Add entry page buttons formatting

--- src/components/pages/EntryPage/EntryPage.js
+++ src/components/pages/EntryPage/EntryPage.js
@@ -1,17 +1,21 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import styles from "./EntryPage.module.scss";
 
 const EntryPage = () => {
-    return(
-        <Container>
-            <Row>
-                <Col md={3} sm={6} xs={6} className={styles.entry}>Polski</Col>
-                <Col md={3} sm={6} xs={6} className={styles.entry}>English</Col>
-            </Row>
-        </Container>
-    )
-}
+  return (
+    <Container className={styles.entrycontainer}>
+      <Row className={styles.entryrow}>
+        <Col md={3} sm={5} xs={5} className={styles.entrybtn}>
+          <h2>Polski</h2>
+        </Col>
+        <Col md={3} sm={5} xs={5} className={styles.entrybtn}>
+          <h2>English</h2>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default EntryPage;
