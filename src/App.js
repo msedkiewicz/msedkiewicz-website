20a444f4a4db251a4589e98d1fa080ccdd68de71
Magdalena Sędkiewicz <magdalenainspirations@gmail.com>
2023-02-20 00:18:57 +0100

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import EntryPage from "./components/pages/EntryPage/EntryPage";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <EntryPage></EntryPage>
+    <>
+      <Routes>
+        <Route path="/" element={<EntryPage />} />
+        {/* <Route path="/pl/" element={<SinglePost />} />
+        <Route path="/pl/o-mnie/" element={<AddPostForm />} />
+        <Route path="/pl/curriculum-vitae/" element={<AddPostForm />} />
+        <Route path="/pl/post/edit/:id" element={<EditPostForm />} />
+        <Route path="/category/" element={<Categories />} />
+        <Route path="/category/:category" element={<CategoryFilter />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </>
   );
 }
 
