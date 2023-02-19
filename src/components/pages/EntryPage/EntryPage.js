729abe98457ed1191f301c950c815a4e9a551e84
Magdalena Sędkiewicz <magdalenainspirations@gmail.com>
2023-02-19 20:18:45 +0100

    Fix entry page styling

--- src/components/pages/EntryPage/EntryPage.js
+++ src/components/pages/EntryPage/EntryPage.js
@@ -5,23 +5,12 @@ import styles from "./EntryPage.module.scss";
 
 const EntryPage = () => {
     return(
-            <Container>
-              <Row>
-                <Col>1 of 2</Col>
-                <Col>2 of 2</Col>
-              </Row>
-              <Row>
-                <Col>1 of 3</Col>
-                <Col>2 of 3</Col>
-                <Col>3 of 3</Col>
-              </Row>
-            </Container>
-        // <Container>
-        //     <Row>
-        //         <Col md={3} sm={6} xs={6} className={styles.entry}>Polski</Col>
-        //         <Col md={3} sm={6} xs={6} className={styles.entry}>English</Col>
-        //     </Row>
-        // </Container>
+        <Container>
+            <Row>
+                <Col md={3} sm={6} xs={6} className={styles.entry}>Polski</Col>
+                <Col md={3} sm={6} xs={6} className={styles.entry}>English</Col>
+            </Row>
+        </Container>
     )
 }
 
