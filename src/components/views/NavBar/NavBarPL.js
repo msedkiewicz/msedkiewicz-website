f23d560fb50fd48e2bc7863805ef9d147df3672e
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-02-21 20:28:08 +0100

    Complete RWD navbar with styles

--- src/components/views/NavBar/NavBarPL.js
+++ src/components/views/NavBar/NavBarPL.js
@@ -1,38 +1,66 @@
 import { Nav, Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import styles from "./NavBar.module.scss";
+import Container from "react-bootstrap/Container";
 
 const NavBarPL = () => {
   return (
-    <Navbar
-      bg="primary"
-      variant="dark"
-      expand="lg"
-      className="mt-4 mb-4 px-4 rounded d-flex justify-content-between"
-    >
-      <Navbar.Brand className="justify-content-start" as={NavLink} to="/pl/">
-        MSedkiewicz
-      </Navbar.Brand>
-      <Nav className="flex-sm-column flex-md-row">
-        <Nav.Link className="px-1" as={NavLink} to="/pl/">
-          Strona główna
-        </Nav.Link>
-        <Nav.Link as={NavLink} to="/pl/o-mnie/">
-          O mnie
-        </Nav.Link>
-        <Nav.Link className="px-1" as={NavLink} to="/pl/curriculum-vitae/">
-          CV
-        </Nav.Link>
-        <Nav.Link className="px-1" as={NavLink} to="/pl/portfolio/">
-          Portfolio
-        </Nav.Link>
-        <Nav.Link className="px-1" as={NavLink} to="/pl/hobby/">
-          Hobby
-        </Nav.Link>
-        <Nav.Link className="px-1" as={NavLink} to="/pl/kontakt/">
-          Kontakt
-        </Nav.Link>
-      </Nav>
-    </Navbar>
+    <div>
+      <Navbar
+        expand="md"
+        variant="dark"
+        className={"mb-4 px-4 flex " + styles.navbar}
+      >
+        <Container>
+          <Navbar.Brand className={styles.navbarbrand} as={NavLink} to="/pl/">
+            MSedkiewicz
+          </Navbar.Brand>
+          <Navbar.Toggle  aria-controls="pl-navbar-nav" />
+          <Navbar.Collapse className={styles.navbarcollapse} id="pl-navbar-nav">
+          <Nav className={"flex-sm-column flex-md-row " + styles.navlinks}>
+            <Nav.Link
+              className={"px-2 " + styles.navlink}
+              as={NavLink}
+              to="/pl/"
+            >
+              Strona główna
+            </Nav.Link>
+            <Nav.Link className={"px-2 " + styles.navlink} as={NavLink} to="/pl/o-mnie/">
+              O mnie
+            </Nav.Link>
+            <Nav.Link
+              className={"px-2 " + styles.navlink}
+              as={NavLink}
+              to="/pl/curriculum-vitae/"
+            >
+              CV
+            </Nav.Link>
+            <Nav.Link
+              className={"px-2 " + styles.navlink}
+              as={NavLink}
+              to="/pl/portfolio/"
+            >
+              Portfolio
+            </Nav.Link>
+            <Nav.Link
+              className={"px-2 " + styles.navlink}
+              as={NavLink}
+              to="/pl/hobby/"
+            >
+              Hobby
+            </Nav.Link>
+            <Nav.Link
+              className={"px-2 " + styles.navlink}
+              as={NavLink}
+              to="/pl/kontakt/"
+            >
+              Kontakt
+            </Nav.Link>
+          </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
 };
 
