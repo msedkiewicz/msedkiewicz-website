ddfe3979f026ba39babf20fcb25560fac1cc442d
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-02-21 20:49:55 +0100

    Add copy for HomePagePL

--- src/components/pages/HomePage/HomePagePL.js
+++ src/components/pages/HomePage/HomePagePL.js
@@ -11,7 +11,10 @@ const HomePagePL = () => {
       <NavBarPL></NavBarPL>
       <Container className={styles.homepagecontainer}>
         <Row className={styles.homepagerow}>
-          <HeroDescription>Hello World!</HeroDescription>
+          <HeroDescription><h1>MSedkiewicz</h1><h2>web developer po psychologii</h2>
+          <p>Do IT ciągnęło mnie jeszcze zanim rozpoczęłam studia na kierunku General Psychology. Po latach zdobywania doświadczenia dziennikarskiego, copywriterskiego, pisarskiego, psychologicznego oraz szermierczego spełniło się moje marzenie o wejściu do IT.</p>
+          <p>Ze wszystkich zajęć, które wykonywałam do tej pory do właśnie kodowanie sprawia mi najwięcej frajdy. Uwielbiam naprawiać błędy w kodzie. Satysfakcję sprawiają mi te momenty, gdy po jakimś czasie pracy z kodem znajduję dobre rozwiązanie dla konkretnego problemu</p>
+          <p>Swoją przygodę rozpoczęłam od WordPressa. Wciąż w nim dłubię, choć obecnie zajmuje mnie bardziej programowanie w React oraz JavaScript.</p></HeroDescription>
           <HeroImage />
         </Row>
       </Container>
