ebefbaf24611347d15e633888b3519d4d6042258
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-02-21 21:22:23 +0100

    Add typography; RWD for typography; navbar styles

--- src/components/pages/HomePage/HomePagePL.js
+++ src/components/pages/HomePage/HomePagePL.js
@@ -11,8 +11,8 @@ const HomePagePL = () => {
       <NavBarPL></NavBarPL>
       <Container className={styles.homepagecontainer}>
         <Row className={styles.homepagerow}>
-          <HeroDescription><h1>MSedkiewicz</h1><h2>web developer po psychologii</h2>
-          <p>Do IT ciągnęło mnie jeszcze zanim rozpoczęłam studia na kierunku General Psychology. Po latach zdobywania doświadczenia dziennikarskiego, copywriterskiego, pisarskiego, psychologicznego oraz szermierczego spełniło się moje marzenie o wejściu do IT.</p>
+          <HeroDescription><h1>Magdalena Sędkiewicz</h1><h2>Web developer po psychologii</h2>
+          <p>Do IT ciągnęło mnie jeszcze zanim rozpoczęłam studia na kierunku General Psychology. Po latach zdobywania doświadczenia dziennikarskiego, copywriterskiego, pisarskiego, psychologicznego oraz szermierczego spełniłam moje marzenie o wejściu do IT.</p>
           <p>Ze wszystkich zajęć, które wykonywałam do tej pory do właśnie kodowanie sprawia mi najwięcej frajdy. Uwielbiam naprawiać błędy w kodzie. Satysfakcję sprawiają mi te momenty, gdy po jakimś czasie pracy z kodem znajduję dobre rozwiązanie dla konkretnego problemu</p>
           <p>Swoją przygodę rozpoczęłam od WordPressa. Wciąż w nim dłubię, choć obecnie zajmuje mnie bardziej programowanie w React oraz JavaScript.</p></HeroDescription>
           <HeroImage />
