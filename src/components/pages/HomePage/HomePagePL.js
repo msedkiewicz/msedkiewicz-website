c7ccff1a4aaab00c1b538301e1550b8a1d663fb4
Magdalena Sędkiewicz <magdalenainspirations@gmail.com>
2023-02-22 13:17:18 +0100

    Cange HomePage description

--- src/components/pages/HomePage/HomePagePL.js
+++ src/components/pages/HomePage/HomePagePL.js
@@ -12,7 +12,8 @@ const HomePagePL = () => {
       <Container className={styles.homepagecontainer}>
         <Row className={styles.homepagerow}>
           <HeroDescription><h1>Magdalena Sędkiewicz</h1><h2>Web developer po psychologii</h2>
-          <p>Do IT ciągnęło mnie jeszcze zanim rozpoczęłam studia na kierunku General Psychology. Po latach zdobywania doświadczenia dziennikarskiego, copywriterskiego, pisarskiego, psychologicznego oraz szermierczego spełniłam moje marzenie o wejściu do IT.</p>
+          <p>Do IT ciągnęło mnie jeszcze zanim rozpoczęłam studia na kierunku General Psychology. Swoją pierwszą stronę na WordPressie postawiłam w 2011 roku. Od tego momentu w wolnym czasie zajmowałam się stronami rodziny i znajomych</p>
+          <p>Po latach zdobywania doświadczenia dziennikarskiego, copywriterskiego, pisarskiego, psychologicznego oraz szermierczego spełniłam moje marzenie o wejściu do IT.</p>
           <p>Ze wszystkich zajęć, które wykonywałam do tej pory do właśnie kodowanie sprawia mi najwięcej frajdy. Uwielbiam naprawiać błędy w kodzie. Satysfakcję sprawiają mi te momenty, gdy po jakimś czasie pracy z kodem znajduję dobre rozwiązanie dla konkretnego problemu</p>
           <p>Swoją przygodę rozpoczęłam od WordPressa. Wciąż w nim dłubię, choć obecnie zajmuje mnie bardziej programowanie w React oraz JavaScript.</p></HeroDescription>
           <HeroImage />
