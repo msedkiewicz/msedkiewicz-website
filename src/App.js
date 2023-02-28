5c3a3d31c79e8dad9e38547ccd29602eca738ece
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-02-28 15:36:13 +0100

    Configure for hosting; add React Helmet

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import { Route, Routes } from "react-router-dom";
 import HomePagePL from "./components/pages/HomePage/HomePagePL";
 import AboutMePL from "./components/pages/AboutMe/AboutMePL";
 import CurriculumPL from "./components/pages/CV/CVPL";
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <>
+    <Helmet>
+      <title>MSedkiewicz - FullStack Developer</title>
+      <meta name="description" content="Magdalena Sędkiewicz - FullStack Developer z Reactowo - Javascriptowym odchyleniem" />
+    </Helmet>
       <Routes>
         <Route path="/" element={<EntryPage />} />
         <Route path="/pl/" element={<HomePagePL />} />
