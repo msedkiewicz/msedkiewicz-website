aab3ded7a706ced28528c7dc306b9535d6b246d9
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-01 17:52:50 +0100

    Change pages styling

--- src/components/features/HeroDescription/HeroDescription.js
+++ src/components/features/HeroDescription/HeroDescription.js
@@ -2,7 +2,7 @@ import Col from "react-bootstrap/Col";
 import styles from "./HeroDescription.module.scss";
 
 const HeroDescription = (props) => {
-    return (<Col md={8} sm={12} xs={12}>{props.children}</Col>)
+    return (<Col md={8} sm={12} xs={12} className={styles.herodescription}>{props.children}</Col>)
 }
 
 export default HeroDescription;
