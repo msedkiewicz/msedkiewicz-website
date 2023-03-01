6fb5fdac733156240761419558e648b04082d6d0
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-01 14:41:06 +0100

    Center HeroImage display

--- src/components/features/HeroImage/HeroImage.js
+++ src/components/features/HeroImage/HeroImage.js
@@ -2,7 +2,7 @@ import Col from "react-bootstrap/Col";
 import styles from "./HeroImage.module.scss"
 import pic from "../../../assets/homepage/magdalena-sedkiewicz-fot-adam-mlodozeniec.jpg"
 const HeroDescription = () => {
-    return (<Col md={4} className={styles.heroimage}>
+    return (<Col md={4} className={'d-flex justify-content-around align-items-start ' + styles.heroimage}>
         <img src={pic} className={styles.imghero} alt="Magdalena Sędkiewicz. Fot.: Adam Młodożeniec"></img>
     </Col>)
 }
