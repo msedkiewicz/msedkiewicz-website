a8e6ca0c42c390bc683397f0ff60250455a4f25a
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-02 00:29:35 +0100

    Fix NavBarEN

--- src/components/views/NavBar/NavBarEN.js
+++ src/components/views/NavBar/NavBarEN.js
@@ -1,38 +1,75 @@
-import { Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-
+import styles from "./NavBar.module.scss";
 const NavBarEN = () => {
   return (
-    <Navbar
-      bg="primary"
-      variant="dark"
-      expand="lg"
-      className="mt-4 mb-4 px-4 rounded d-flex justify-content-between"
-    >
-      <Navbar.Brand className="justify-content-start" as={NavLink} to="/en/">
-        MSedkiewicz
-      </Navbar.Brand>
-      <Nav className="flex-sm-column flex-md-row">
-        <Nav.Link className="px-1" as={NavLink} to="/en/">
-          Home
-        </Nav.Link>
-        <Nav.Link as={NavLink} to="/en/about-me/">
-          About
-        </Nav.Link>
-        <Nav.Link className="px-1" as={NavLink} to="/en/curriculum-vitae/">
-          CV
-        </Nav.Link>
-        <Nav.Link className="px-1" as={NavLink} to="/en/portfolio/">
-          Portfolio
-        </Nav.Link>
-        <Nav.Link className="px-1" as={NavLink} to="/en/hobbies/">
-          Hobbies
-        </Nav.Link>
-        <Nav.Link className="px-1" as={NavLink} to="/en/contact/">
-          Contact
-        </Nav.Link>
-      </Nav>
-    </Navbar>
+    <div>
+      <Navbar
+        expand="md"
+        variant="dark"
+        className={"mb-4 px-4 flex " + styles.navbar}
+      >
+        <Container>
+          <Navbar.Brand
+            className={styles.navbarbrand}
+            as={NavLink}
+            to="/en/"
+          >
+            MSedkiewicz
+          </Navbar.Brand>
+          <Navbar.Toggle  aria-controls="pl-navbar-nav" />
+          <Navbar.Collapse className={styles.navbarcollapse} id="pl-navbar-nav">
+          <Nav className={"flex-sm-column flex-md-row " + styles.navlinks}>
+            <Nav.Link
+              className={"px-2 " + styles.navlink}
+              as={NavLink}
+              to="/en/"
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link className={"px-2 " + styles.navlink} as={NavLink} to="/en/about-me/">
+              About me
+            </Nav.Link>
+            <Nav.Link
+              className={"px-2 " + styles.navlink}
+              as={NavLink}
+              to="/en/curriculum-vitae/"
+            >
+              CV
+            </Nav.Link>
+            <Nav.Link
+              className={"px-2 " + styles.navlink}
+              as={NavLink}
+              to="/en/portfolio/"
+            >
+              Portfolio
+            </Nav.Link>
+            <Nav.Link
+              className={"px-2 " + styles.navlink}
+              as={NavLink}
+              to="/en/hobbies/"
+            >
+              Hobbies
+            </Nav.Link>
+            <Nav.Link
+              className={"px-2 " + styles.navlink}
+              as={NavLink}
+              to="/en/contact/"
+            >
+              Contact
+            </Nav.Link>
+            <Nav.Link
+              className={"px-2 " + styles.navlinklanguage}
+              as={NavLink}
+              to="/pl/"
+            >
+              <b>PL</b>
+            </Nav.Link>
+          </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
 };
 
